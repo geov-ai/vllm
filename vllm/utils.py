21edec7cf7f09c76f8a37dc543865cf758e83d91
Varuna Jayasiri <vpjayasiri@gmail.com>
2025-01-20 13:44:21 +0530

    tensor h2d assert

--- vllm/utils.py
+++ vllm/utils.py
@@ -764,7 +764,8 @@ def async_tensor_h2d(
 ) -> torch.Tensor:
     if target_device == "cuda":
         target_device = torch.cuda.current_device()
-    assert torch.cuda.current_device() == target_device, f'{target_device} != {torch.cuda.current_device()}'
+    assert torch.cuda.current_device() == target_device, \
+        f'{type(target_device)} {repr(target_device)} != {repr(torch.cuda.current_device())}'
     """Asynchronously create a tensor and copy it from host to device."""
     t = torch.tensor(data, dtype=dtype, pin_memory=pin_memory, device="cpu")
     return t.to(device=target_device, non_blocking=True)
